c56404b248a412ff5784c83347b36a64e53fd425
Gotox29 <anthony.nizac@gmail.com>
2018-06-02 15:33:50 +0200

    test commit via eclipse

--- GSBRH/src/GSBRH.java
+++ GSBRH/src/GSBRH.java
@@ -15,9 +15,6 @@ public class GSBRH {
 	 *            Passe un tableau d'argument a l'application
 	 */
 	public static void main(String[] args) {
-		for (int i = 0; i < args.length; i++) {
-			System.out.print(args[i]);
-		}
 		Controleur controleur = new Controleur();
 		controleur.lancerAppli();
 	}
